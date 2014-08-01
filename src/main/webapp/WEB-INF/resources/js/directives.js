119a54f0c9a715ba15ffb7cfcdfe7d393c7c2676
gcanobbio <g.canobbio@gmail.com>
2014-08-01 11:37:33 +0200

    Issue #2 solved: after click delete, a small dialog is opened.

--- src/main/webapp/WEB-INF/resources/js/directives.js
+++ src/main/webapp/WEB-INF/resources/js/directives.js
@@ -100,3 +100,29 @@ directives.directive('integer', function() {
         }
     };
 });
+
+/**
+* A generic confirmation for risky actions.
+* Usage: Add attributes:
+* * ng-confirm-message="Are you sure?"
+* * ng-confirm-click="takeAction()" function
+* * ng-confirm-condition="mustBeEvaluatedToTrueForTheConfirmBoxBeShown" expression
+*/
+directives.directive('ngConfirmClick', [ function() {
+	return {
+		restrict : 'A',
+		link : function(scope, element, attrs) {
+			element.bind('click', function() {
+				var condition = scope.$eval(attrs.ngConfirmCondition);
+				if (condition) {
+					var message = attrs.ngConfirmMessage;
+					if (message && confirm(message)) {
+						scope.$apply(attrs.ngConfirmClick);
+					}
+				} else {
+					scope.$apply(attrs.ngConfirmClick);
+				}
+			});
+		}
+	};
+} ]);
