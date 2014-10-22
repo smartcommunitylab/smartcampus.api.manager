73a4867ec74c73214b17bd73cec9561b3536300d
gcanobbio <g.canobbio@gmail.com>
2014-10-22 14:34:54 +0200

    Issue #9: policy ip access control - new logic in proxy to apply policy.

--- src/main/java/eu/trentorise/smartcampus/api/manager/proxy/IPAccessControlApply.java
+++ src/main/java/eu/trentorise/smartcampus/api/manager/proxy/IPAccessControlApply.java
@@ -15,6 +15,9 @@
  ******************************************************************************/
 package eu.trentorise.smartcampus.api.manager.proxy;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -104,7 +107,7 @@ public class IPAccessControlApply implements PolicyDatastoreApply{
 	public  boolean IpAccessControlDecision(List<SourceAddress> BL, List<SourceAddress> WL, 
 			String appIp, String noRuleMatchAction) {	
 		
-		String appIp_24=appIpMask(appIp)[0]; 
+		/*String appIp_24=appIpMask(appIp)[0]; 
 		String appIp_16=appIpMask(appIp)[1]; 
 		
 		boolean condBL=BL.contains(appIp) || BL.contains(appIp_24) || BL.contains(appIp_16);
@@ -124,6 +127,34 @@ public class IPAccessControlApply implements PolicyDatastoreApply{
 			}else
 				decision= false;
 		}
+		return decision;*/
+		
+		boolean decision = false;
+
+		boolean BL_or = false;
+		boolean WL_or = false;
+
+		try {
+			for (int i = 0; i < BL.size(); i++) {
+				BL_or = BL_or || IP(BL.get(i).getMask(), ip2ipbv(BL.get(i).getIp()), ip2ipbv(appIp));
+			}
+			for (int i = 0; i < WL.size(); i++) {
+				WL_or = WL_or || IP(WL.get(i).getMask(), ip2ipbv(WL.get(i).getIp()), ip2ipbv(appIp));
+			}
+		} catch (UnknownHostException e) {
+			return false;
+		}
+
+		System.out.print("BL_or=" + BL_or + "\n");
+		if (noRuleMatchAction.equalsIgnoreCase("ALLOW")) {
+			if ((!BL_or) || WL_or)
+				decision = true;
+		}
+		if (noRuleMatchAction.equalsIgnoreCase("DENY")) {
+			if ((!BL_or) && WL_or)
+				decision = true;
+		}
+
 		return decision;
 	}
 
@@ -133,7 +164,7 @@ public class IPAccessControlApply implements PolicyDatastoreApply{
 	 * @param appIp : String
 	 * @return string value of ip
 	 */
-	private String[] appIpMask(String appIp){
+	/*private String[] appIpMask(String appIp){
 	
 		String contrary=new StringBuilder(appIp).reverse().toString();
 		int endIndex = appIp.length()-contrary.indexOf('.')-1;	
@@ -144,6 +175,63 @@ public class IPAccessControlApply implements PolicyDatastoreApply{
 		String appIp_16 =appIp.substring(0, endIndex2 );
 		String[] ip=new String[]{appIp_24,appIp_16};
 		return ip;
+	}*/
+	
+	/**
+	 * Retrieve int representation of ip address.
+	 * 
+	 * @param ip : String
+	 * @return int : ip
+	 * @throws UnknownHostException 
+	 */
+	private int ip2ipbv(String ip) throws UnknownHostException{
+		/*int[] index = new int[3];
+		int j = 0;
+		for (int i = 0; i < ip.length(); i++) {
+			if (ip.charAt(i) == '.') {
+				index[j] = i;
+				j = j + 1;
+			}
+		}
+		int ip_a = Integer.parseInt(ip.substring(0, index[0]));
+		int ip_b = Integer.parseInt(ip.substring(index[0] + 1, index[1]));
+		int ip_c = Integer.parseInt(ip.substring(index[1] + 1, index[2]));
+		int ip_d = Integer.parseInt(ip.substring(index[2] + 1));
+		*/
+		Inet4Address a = (Inet4Address) InetAddress.getByName(ip);
+		byte[] b = a.getAddress();
+		int ip_bv = ((b[0] & 0xFF) << 24) |
+                 	((b[1] & 0xFF) << 16) |
+                 	((b[2] & 0xFF) << 8)  |
+                 	((b[3] & 0xFF) << 0);
+
+		//int ip_bv = ip_a << 24 | ip_b << 16 | ip_c << 8 | ip_d; // concat two int of 8 bits
+		return ip_bv;
+	}
+		
+				
+	/**
+	 * Check if app ip is in range of ip with mask.
+	 * 
+	 * @param mask : int
+	 * @param ip : int
+	 * @param appIp : int
+	 * @return boolean : true if app ip is in range, otherwise false
+	 */
+	private boolean IP(int mask, int ip, int appIp) {
+		int min24 = 255 << 24 | 255 << 16 | 255 << 8 | 0;
+		int min16 = 255 << 24 | 255 << 16 | 0 << 8 | 0;
+		Boolean result = false;
+
+		if (mask == 32 && appIp == ip) {
+			result = true;
+		} else if (mask == 24 && (ip & min24) < appIp && appIp < (ip | 255)) {
+			result = true;
+		} else if (mask == 16 && (ip & min16) < appIp && appIp < (ip | 65535)) {
+
+			result = true;
+		}
+		return result;
 	}
 
 	
